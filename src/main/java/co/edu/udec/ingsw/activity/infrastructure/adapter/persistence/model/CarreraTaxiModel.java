eff653ecfcc7b05a137b787e22b2df564e988534
Eugenio <geustodj@gmail.com>
2025-10-17 17:42:15 -0500

    Create config & model files for skeeleton
    Creación de archivos vacíos con proposito de estructura cuando se cambie la tecnología de la persistencia

---
+++ src/main/java/co/edu/udec/ingsw/activity/infrastructure/adapter/persistence/model/CarreraTaxiModel.java
@@ -0,0 +1,5 @@
+package co.edu.udec.ingsw.activity.infrastructure.adapter.persistence.model;
+/**
+ * Modelo de persistencia para la entidad CarreraTaxi. será necesario para la persistencia de la entidad en la base de datos.
+ * Se utulizará para cuando se cambie de base de datos.
+ */
