ce7529bd0c02114a8d9097a416abcdf4c89bfe98
Eugenio <geustodj@gmail.com>
2025-11-03 22:12:18 -0500

    Create UsuarioRepositoryPort.java
    implementación de puerto de salida para entidad usuario (inicio de capa de aplicación)

---
+++ src/main/java/co/edu/udec/ingsw/activity/application/usuario/port/out/UsuarioRepositoryPort.java
@@ -0,0 +1,14 @@
+package co.edu.udec.ingsw.activity.application.usuario.port.out;
+
+import co.edu.udec.ingsw.activity.domain.usuario.entity.Usuario;
+import co.edu.udec.ingsw.activity.domain.usuario.valueobject.UsuarioId;
+import java.util.List;
+
+public interface UsuarioRepositoryPort {
+  UsuarioId nextId();
+  void create(Usuario usuario);
+  void update(Usuario usuario);
+  void delete(UsuarioId id);
+  Usuario findById(UsuarioId id);
+  List<Usuario> findAll();
+}
